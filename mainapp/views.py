eded103b8333e78d4dc3d25a06774f77bd7eae80
Kolomatskiy Artem <a.kolomatskiy@rambler-co.ru>
2019-10-17 20:49:13 +0300

    fix products

--- mainapp/views.py
+++ mainapp/views.py
@@ -46,11 +46,11 @@ def products(request, pk=None, page=1):
                 'pk': 0,
                 'name': 'все'
             }
-            products = Product.objects.filter(is_active=True, category__is_active=True).order_by('price')
+            products = Product.objects.filter(is_active=True, category__is_active=True).order_by('price').select_related('category')
         else:
             category = get_object_or_404(ProductCategory, pk=pk)
             products = Product.objects.filter(category__pk=pk, is_active=True, category__is_active=True).order_by(
-                'price')
+                'price').select_related('category')
 
         paginator = Paginator(products, 2)
         try:
